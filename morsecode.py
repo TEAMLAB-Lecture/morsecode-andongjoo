f008d894d8af4674e7a25569d5fa7b18efe1ca16
안동주 <andongjoo1@naver.com>
2021-01-22 05:51:59 +0900

    update morce 2

--- morsecode.py
+++ morsecode.py
@@ -88,11 +88,11 @@ def is_validated_english_sentence(user_input):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result = True
-    if user_input=="":
+    if not user_input:
         result=False
     
     for i in user_input:
-        if not i.isalpha():
+        if not i.isalpha() and i!=' ':
             result=False
 
     return result
