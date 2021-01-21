96e71e471d312e5bbded9d84b466b2f72dc5d020
안동주 <andongjoo1@naver.com>
2021-01-22 06:07:44 +0900

    update morce 5

--- morsecode.py
+++ morsecode.py
@@ -88,8 +88,9 @@ def is_validated_english_sentence(user_input):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result=True
+    nonAlpha = "0123456789_@#$%^&*()-+=[]}{';:\|`~\""
     for i in user_input:
-        if i in unavailable:
+        if i in nonAlpha:
             result=False
     if not user_input.replace('.','').replace(',','').replace('!','').replace('?','').replace(' ',''):
         result=False
